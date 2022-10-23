060b5af732c91230fd99c49cca7869d04fc2dd04
Mah <marinalarissacarpesrohrig@gmail.com>
2022-10-23 13:10:46 -0300

    criado model person

---
+++ src/main/java/br/com/futurodev/crud/models/Person.java
@@ -0,0 +1,103 @@
+package br.com.futurodev.crud.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "Person")
+public class Person implements Serializable {
+
+    @Id
+    private Long id;
+    private Date birthDate;
+    private LocalTime timeOfBirth;
+    private String email;
+    private String state;
+    private String city;
+    private String street;
+    private String cep;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id.equals(person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalTime getTimeOfBirth() {
+        return timeOfBirth;
+    }
+
+    public void setTimeOfBirth(LocalTime timeOfBirth) {
+        this.timeOfBirth = timeOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
