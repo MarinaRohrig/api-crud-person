eb62836c32c3df0b8832ed8d545ff692ab21c1f6
Mah <marinalarissacarpesrohrig@gmail.com>
2022-10-23 13:39:25 -0300

    criado person controller ~

--- src/main/java/br/com/futurodev/crud/repository/PersonRepository.java
+++ src/main/java/br/com/futurodev/crud/repository/PersonRepository.java
@@ -1,10 +1,10 @@
 package br.com.futurodev.crud.repository;
 
 import br.com.futurodev.crud.models.Person;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends CrudRepository <Person, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
